8011cdfb536e40da4531fb0685a9866f944bfe2d
joeygaitan <joeygaitan707@gmail.com>
2021-07-25 18:55:21 -0700

    adding authentication

--- api/utils/authentication.js
+++ api/utils/authentication.js
@@ -2,5 +2,33 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 module.exports = {
-    Authenticate: function ()
+    Authenticate: function ({ req }) 
+    {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token)
+        {
+            return req
+        }
+
+        try 
+        {
+            const { data } = jwt.verify(token, secret, { maxAge: process.env.EXPIRATION })
+            req.user = data
+        }
+        catch
+        {
+            console.log("Wrong token :/")
+        }
+    },
+
+    CreateToken: function ({username, email, id})
+    {
+        return jwt.sign({data: {username, email, id}}, process.env.SECRET, { expiresIn: process.env.EXPIRATION })
+    }
 };
