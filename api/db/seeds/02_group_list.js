55d289172ce0d057a2bff6527526ab7e505d0cce
joeygaitan <joeygaitan>
2021-08-17 02:47:19 -0700

    set up accepting invitations to follow a list. Reworked getting lists and added in the username for a list you follow to be displayed. Allowed add element to also double as a update one. Debating on just adding a another mutation for this.....

--- api/db/seeds/02_group_list.js
+++ api/db/seeds/02_group_list.js
@@ -4,9 +4,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('group_list').insert([
-        { user_id: 1, name:"airsoft gear", password:"$2b$10$OzO690CMDy9uELhbkgxe0uBAI3DAFTk7wcgdGXax2wDimoscXubZ2", private:true },
-        { user_id: 1, name:"homework", password:"$2b$10$OzO690CMDy9uELhbkgxe0uBAI3DAFTk7wcgdGXax2wDimoscXubZ2", private:false },
-        { user_id: 1, name:"adventures", password:"$2b$10$OzO690CMDy9uELhbkgxe0uBAI3DAFTk7wcgdGXax2wDimoscXubZ2", private:false }
+        { user_id: 1, name:"airsoft gear", private:true },
+        { user_id: 1, name:"homework", private:false },
+        { user_id: 1, name:"adventures", private:false }
       ]);
     });
 };
